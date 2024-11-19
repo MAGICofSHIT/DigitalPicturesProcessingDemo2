aa55f3e5c8c232c62052d0dea4afbd7724eb6d34
MAGICofSHIT <1271384735@qq.com>
2024-11-19 15:30:42 +0800

    先前傅里叶变换关系错误，这次已修正

--- main.py
+++ main.py
@@ -29,41 +29,42 @@ if __name__ == "__main__":
     F = np.fft.fft2(image)
     # F = FT(image)
 
+    # 计算频谱的幅度
+    original_magnitude_spectrum = np.abs(F)
+
     # 频谱移位，使得低频部分移动到中心
-    Fshift = np.fft.fftshift(F)
+    # Fshift = np.fft.fftshift(F)
 
     # 保存原始频谱图
     plt.figure(figsize=(6, 6))
-    plt.imshow(np.abs(Fshift), cmap='gray')
+    plt.imshow(original_magnitude_spectrum, cmap='gray')
     plt.axis('off')
     plt.savefig('./Pictures/original_frequency_spectrum.png', dpi=300, bbox_inches='tight', pad_inches=0)
     plt.close()  # 关闭当前图形
 
-    # 计算频谱的幅度并取对数，以便更容易可视化
-    magnitude_spectrum = np.abs(F)
-    magnitude_spectrum = np.log(magnitude_spectrum + 1)
-
-    # 保存频谱图
-    plt.figure(figsize=(6, 6))
-    plt.imshow(magnitude_spectrum, cmap='gray')
-    plt.axis('off')
-    plt.savefig('./Pictures/log_frequency_spectrum.png', dpi=300, bbox_inches='tight', pad_inches=0)
-    plt.close()  # 关闭当前图形
-
-    # 对图像进行中心化处理：乘以 (-1)^(x+y)
+    # 对图像进行中心化处理：乘以(-1) ^ (x + y)
     centered_image = centerImage(image)
 
-    # 计算中心化后的傅里叶变换
+    # 计算傅里叶变换
     F_centered = np.fft.fft2(centered_image)
-    # F_centered = FT(centered_image)
 
-    # 计算频谱的幅度并取对数，以便更容易可视化
-    magnitude_spectrum_centered = np.abs(F_centered)
-    magnitude_spectrum_centered = np.log(magnitude_spectrum_centered + 1)
+    # 计算频谱的幅度
+    centered_magnitude_spectrum = np.abs(F_centered)
 
     # 保存频谱图
     plt.figure(figsize=(6, 6))
-    plt.imshow(magnitude_spectrum_centered, cmap='gray')
+    plt.imshow(centered_magnitude_spectrum, cmap='gray')
     plt.axis('off')
     plt.savefig('./Pictures/centered_frequency_spectrum.png', dpi=300, bbox_inches='tight', pad_inches=0)
+    plt.close()  # 关闭当前图形
+
+
+    # 计算频谱的幅度并取对数，以便更容易可视化
+    log_centered_magnitude_spectrum = np.log(centered_magnitude_spectrum + 1)
+
+    # 保存频谱图
+    plt.figure(figsize=(6, 6))
+    plt.imshow(log_centered_magnitude_spectrum, cmap='gray')
+    plt.axis('off')
+    plt.savefig('./Pictures/log_centered_frequency_spectrum.png', dpi=300, bbox_inches='tight', pad_inches=0)
     plt.close()  # 关闭当前图形
